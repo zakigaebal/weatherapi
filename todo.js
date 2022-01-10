a74384915cd25966e56aefdc72cd177733fd7054
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-10 11:47:31 +0900

    asdasda

--- todo.js
+++ todo.js
@@ -1,69 +1,94 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = toDoForm. querySelector("input"),
-    toDoList = document.querySelector(".js-toDoList");
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-function filterFn(toDo){
-    return toDo.id === 1
+function filterFn(toDo) {
+  return toDo.id === 1;
 }
 
 let toDos = [];
 
-function deleteToDo(event){
-    const btn= event.target;
-    const li = btn.parentNode;
-    toDoList.removeChild(li);
-    const cleanToDos = toDos.filter(function(toDo){
-       return toDo.id !== parseInt(li.id) ;
-    });
-    toDos = cleanToDos
-    saveToDos();
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
-function saveToDos(){
-    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-function paintToDo(text){
-    const li = document.createElement("li");
-    const delBtn = document.createElement("button");
-    const span = document.createElement("span");
-    const newId = toDos.length + 1;
-    delBtn.innerHTML  = "✓"
-    delBtn.addEventListener("click", deleteToDo);
-    span.innerText = text;
-    li.appendChild(delBtn);
-    li.appendChild(span);
-    li.id = newId;
-    toDoList.appendChild(li);
-    const toDoObj = {
-        text: text,
-        id: newId
-    };
-    toDos.push(toDoObj);
-    saveToDos();
+function paintToDo(text) {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "checkbox";
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "";
+    }
+  });
+
+  let today = new Date();
+
+  let time = document.createElement("h6");
+  time.textContent =
+    String(today.getHours()) + ":" + String(today.getMinutes());
+
+  const li = document.createElement("li");
+  li.id = "listing";
+
+  const delBtn = document.createElement("button");
+  delBtn.id = "box1";
+  const span = document.createElement("span");
+  span.id = "box1";
+  const newId = toDos.length + 1;
+  delBtn.innerHTML = "제거하기";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(checkbox);
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
+
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+  li.appendChild(time);
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
-  if(loadedToDos !== null){
-      const parsedToDos = JSON.parse(loadedToDos);
-      parsedToDos.forEach(function(toDo){
-          paintToDo(toDo.text);
-      })
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
 function init() {
-  loadToDos( );
+  loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
--- weather.js
+++ weather.js
@@ -2,9 +2,9 @@ const weather = document.querySelector(".js-weather");
 
 const API_KEY = "aa6ceff75ee966bc45ddbfba4edf90b1";
 
-function getWeather(lat, lng) {
+function getWeather() {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`
+    `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`
   )
     .then(function (respone) {
       return respone.json();
@@ -12,41 +12,13 @@ function getWeather(lat, lng) {
     .then(function (json) {
       const temperature = json.main.temp;
       const place = json.name;
-      weather.innerText = `${place}의 온도는  ${temperature}`;
+      weather.innerText = `서울의 현재 온도는  ${Math.ceil(
+        temperature - 273.15
+      )}도`;
     });
 }
 
-function saveCoords(coordsObj) {
-  localStorage.setItem("coords", JSON.stringify(coordsObj));
-}
-
-function handleGeoSuccess(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const coordsObj = {
-    //객체에 변수의 이름과 객체의 키가 같으면 그냥ㅇ 이렇게 할 수 있음
-    latitude,
-    longitude,
-  };
-  saveCoords(coordsObj);
-  getWeather(latitude, longitude);
-}
-function handleGeoError() {
-  console.log("cant access geo location");
-}
-function askForCoords() {
-  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
-}
-function loadCoords() {
-  const loadCoords = localStorage.getItem("coords");
-  if (loadCoords === null) {
-    askForCoords();
-  } else {
-    const parseCoords = JSON.parse(loadCoords);
-    getWeather(parseCoords.latitude, parseCoords.longitude);
-  }
-}
 function init() {
-  loadCoords();
+  getWeather();
 }
 init();
